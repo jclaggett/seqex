03b302ce7c96b8d6ae4d80cf70c2f0b856209267
Jonathan Claggett <jonathan@claggett.org>
2013-01-25 01:01:47 -0500

    Update project.clj to remove explicit deps and add profiles.

--- project.clj
+++ project.clj
@@ -1,12 +1,23 @@
-(defproject sc "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.5.0-beta1"]
-                 [criterium "0.3.1"]]
+(defproject seqex "1.0.0-SNAPSHOT"
+            :description "Sequence Expressions."
+            :url "http://github.com/jclaggett/seqex"
+            :license {:name "Eclipse Public License - v 1.0"
+                      :url "http://www.eclipse.org/legal/epl-v10.html"
+                      :distribution :repo
+                      :comments "same as Clojure"}
+            :min-lein-version "2.0.0"
 
-  ;; Describe a performance specific test profile
-  :test-selectors {:default (complement :perf), :perf :perf}
+            ;; Describe a performance specific test profile
+            :test-selectors {:default (complement :perf), :perf :perf}
 
-  ;; Use this to allow YourKit to connect:
-  ;; :jvm-opts ["-agentpath:yjp/libyjpagent.so"]
-  )
-
+            :profiles {:1.2 {:dependencies [[criterium "0.3.1"] [org.clojure/clojure "1.2.0"]]}
+                       :1.3 {:dependencies [[criterium "0.3.1"] [org.clojure/clojure "1.3.0"]]}
+                       :1.4 {:dependencies [[criterium "0.3.1"] [org.clojure/clojure "1.4.0"]]}
+                       :1.5 {:dependencies [[criterium "0.3.1"] [org.clojure/clojure "1.5.0-RC2"]]}}
+            :aliases {"1.2" ["with-profile" "1.2"]
+                      "1.3" ["with-profile" "1.3"]
+                      "1.4" ["with-profile" "1.4"]
+                      "1.5" ["with-profile" "1.5"]}
+            ;; Use this to allow YourKit to connect:
+            ;; :jvm-opts ["-agentpath:yjp/libyjpagent.so"]
+            )
