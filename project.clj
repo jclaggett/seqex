4c6eebfc9861af5839e8ffe365638e5556a10e6b
Jonathan Claggett <jonathan@claggett.org>
2013-07-10 23:36:41 -0400

    Update project.clj to default to Clojure 1.5.1

--- project.clj
+++ project.clj
@@ -7,17 +7,19 @@
                       :comments "same as Clojure"}
             :min-lein-version "2.0.0"
 
+            :dependencies [[criterium "0.3.1"] [org.clojure/clojure "1.5.1"]]
+
             ;; Describe a performance specific test profile
             :test-selectors {:default (complement :perf), :perf :perf}
 
-            :profiles {:1.2 {:dependencies [[criterium "0.3.1"] [org.clojure/clojure "1.2.0"]]}
-                       :1.3 {:dependencies [[criterium "0.3.1"] [org.clojure/clojure "1.3.0"]]}
-                       :1.4 {:dependencies [[criterium "0.3.1"] [org.clojure/clojure "1.4.0"]]}
-                       :1.5 {:dependencies [[criterium "0.3.1"] [org.clojure/clojure "1.5.0-RC2"]]}}
-            :aliases {"1.2" ["with-profile" "1.2"]
-                      "1.3" ["with-profile" "1.3"]
+            :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+                       :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+                       :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+                       :1.2 {:dependencies [[org.clojure/clojure "1.2.0"]]}}
+            :aliases {"1.5" ["with-profile" "1.5"]
                       "1.4" ["with-profile" "1.4"]
-                      "1.5" ["with-profile" "1.5"]}
+                      "1.3" ["with-profile" "1.3"]
+                      "1.2" ["with-profile" "1.2"]}
             ;; Use this to allow YourKit to connect:
             ;; :jvm-opts ["-agentpath:yjp/libyjpagent.so"]
             )
