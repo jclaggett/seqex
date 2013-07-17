ff9113321424acc82d5249661f777dc80ed6cef1
Jonathan Claggett <jonathan@claggett.org>
2013-07-17 15:13:34 -0400

    Update math example to capture form.

--- examples/math.clj
+++ examples/math.clj
@@ -2,6 +2,7 @@
   (:require [n01se.seqex :as se]))
 
 ;; math expressions demo
+;; Match basic standard infix notation and capture the results as a list.
 (def ws
   "Arbitrary amount of whitespace."
   (se/qty* \space \tab))
@@ -18,30 +19,43 @@
      (se/ord leader (se/qty* (apply se/ord following)))))
 
 (def digits
-  "one or more digits."
+  "One or more digits."
   (apply se/qty+ "0123456789"))
 
 (def number
-  "real number. (captured)"
-  (se/recap #(Double/parseDouble (apply str %))
-            (se/cap-tokens
-              (se/ord (se/opt \+ \-)
-                      (se/alt digits
-                              (se/ord \. digits)
-                              (se/ord digits \.)
-                              (se/ord digits \. digits))
-                      (se/opt (se/ord (se/alt \e \E)
-                                      (se/opt \+ \-)
-                                      digits))))))
+  "Real number. Captured and converted to a double."
+  (se/cap (se/ord (se/opt \+ \-)
+                  (se/alt digits
+                          (se/ord \. digits)
+                          (se/ord digits \.)
+                          (se/ord digits \. digits))
+                  (se/opt (se/ord (se/alt \e \E)
+                                  (se/opt \+ \-)
+                                  digits)))
+          #(Double/parseDouble (apply str %))))
 
-(declare add-expr)
-(def atom-expr (se/alt number (ord-ws \( (delay add-expr) \) )))
-(def pow-expr (first-rest* atom-expr \^ atom-expr))
-(def mul-expr (first-rest* pow-expr (se/alt \* \/) pow-expr))
-(def add-expr (first-rest* mul-expr (se/alt \+ \-) mul-expr))
+(declare add-expr) ;; allows paren-expr to refer to add-expr
+(def paren-expr
+  "Parens wrapping an expression. Recaptured in a list."
+  (se/recap (ord-ws \( (delay add-expr) \))
+            list))
+(defn op
+  "Operators. Captured and converted into symbols."
+  [& op-chars]
+  (se/cap-one (apply se/alt op-chars)
+              (comp symbol str)))
+(def atom-expr (se/alt number paren-expr))
+(def pow-expr (first-rest* atom-expr (se/cap (se/ord \p \o \w)
+                                         (comp symbol (partial apply str)))
+                atom-expr))
+(def mul-expr (first-rest* pow-expr (op \* \/) pow-expr))
+(def add-expr (first-rest* mul-expr (op \+ \-) mul-expr))
 (def math-expr add-expr)
 
-(def big-example "2^(2+2) * (-1/(0--1) - 12.3)")
+(def big-example "2 pow (.2 + +2e+3) * (-1.E-1/(0--1) - 12.3)")
 
-(se/valid? math-expr big-example) ;=> true
-(se/model math-expr big-example) ;=> (2.0 2.0 2.0 -1.0 0.0 -1.0 12.3)
+(assert (= true
+           (se/valid? math-expr big-example)))
+(assert (= '(2.0 pow (0.2 + 2000.0) * (-0.1 / (0.0 - -1.0) - 12.3))
+           (se/model math-expr big-example)))
+
