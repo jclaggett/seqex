dda77398b0657f181559c257b55ac8ca2f9db28a
Jonathan Claggett <jonathan@claggett.org>
2013-07-19 10:02:46 -0400

    Refactor math example to calculate answer.

--- examples/math.clj
+++ examples/math.clj
@@ -1,29 +1,20 @@
 (ns n01se.seqex.examples.math
   (:require [n01se.seqex :as se]))
 
-;; math expressions demo
-;; Match basic standard infix notation and capture the results as a list.
+;; Math Expressions Demo
+;; Match basic standard infix notation and capture the results as a Clojure
+;; expression that evaluates to the answer.
+
 (def ws
   "Arbitrary amount of whitespace."
   (se/qty* \space \tab))
-
-(defn ord-ws
-  "Ordered seqexes interposed with whitespace."
-  [& seqexes] (apply se/ord (interpose ws seqexes)))
-
-(defn first-rest*
-  "Ordered seqexes interposed with whitespace where the first seqex is required
-  and all following seqexes are repeated zero or more times."
-  [& seqexes]
-   (let [[leader & following] (interpose ws seqexes)]
-     (se/ord leader (se/qty* (apply se/ord following)))))
 
 (def digits
   "One or more digits."
   (apply se/qty+ "0123456789"))
 
 (def number
-  "Real number. Captured and converted to a double."
+  "Real number. Captured as a double."
   (se/cap (se/ord (se/opt \+ \-)
                   (se/alt digits
                           (se/ord \. digits)
@@ -34,28 +25,45 @@
                                   digits)))
           #(Double/parseDouble (apply str %))))
 
-(declare add-expr) ;; allows paren-expr to refer to add-expr
-(def paren-expr
-  "Parens wrapping an expression. Recaptured in a list."
-  (se/recap (ord-ws \( (delay add-expr) \))
-            list))
 (defn op
-  "Operators. Captured and converted into symbols."
-  [& op-chars]
-  (se/cap-one (apply se/alt op-chars)
-              (comp symbol str)))
-(def atom-expr (se/alt number paren-expr))
-(def pow-expr (first-rest* atom-expr (se/cap (se/ord \p \o \w)
-                                         (comp symbol (partial apply str)))
-                atom-expr))
-(def mul-expr (first-rest* pow-expr (op \* \/) pow-expr))
-(def add-expr (first-rest* mul-expr (op \+ \-) mul-expr))
+  "Operator. Captured as function."
+  [literal func]
+  (se/cap-one literal #(do %1 func)))
+
+(def -sub (op \- -))
+(def -add (op \+ +))
+(def -mul (op \* *))
+(def -div (op \/ /))
+(def -mod (op \% mod))
+(def -pow (op \^ #(Math/pow %1 %2)))
+
+(defn bin-expr
+  "Left to right associative, infix binary expressions. Captured as
+  S-expressions."
+  [expr & ops]
+  (se/recap (se/ord expr ws
+                (se/qty* (se/recap (se/ord (apply se/alt ops) ws expr)
+                             ;; wrap the op and expr in a list
+                             list)))
+      ;; Take leading expr and following '(op expr) pairs and nest them.
+      (fn [models]
+        (list (reduce #(cons (first %2)
+                             (cons %1 (rest %2)))
+                      models)))))
+
+(declare add-expr) ;; allows atom-expr to refer to add-expr
+(def atom-expr (se/alt number (se/ord \( ws (delay add-expr) ws \))))
+(def pow-expr (bin-expr atom-expr -pow))
+(def mul-expr (bin-expr pow-expr -mul -div -mod))
+(def add-expr (bin-expr mul-expr -add -sub))
 (def math-expr add-expr)
 
-(def big-example "2 pow (.2 + +2e+3) * (-1.E-1/(0--1) - 12.3)")
+(def big-example "2^(.2 + +2e+0) * (-1.E-1/(0--1) - 12.3)")
 
 (assert (= true
            (se/valid? math-expr big-example)))
-(assert (= '(2.0 pow (0.2 + 2000.0) * (-0.1 / (0.0 - -1.0) - 12.3))
-           (se/model math-expr big-example)))
+
+(assert (= (* (Math/pow 2 (+ 0.2 2e+0)) (- (/ -1.0E-1 (- 0 -1)) 12.3))
+           (-> (se/model math-expr big-example)
+               first eval)))
 
