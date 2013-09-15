c0a75d4b701658b9a29eb23acf0a32c2a101d05c
Jonathan Claggett <jonathan@claggett.org>
2013-09-15 14:57:05 -0400

    Remove experimental error code.

--- examples/math.clj
+++ examples/math.clj
@@ -67,19 +67,3 @@
            (-> (se/model math-expr big-example)
                first eval)))
 
-(defn get-output-type [x]
-  (:output (meta x)))
-
-(defn original?
-  "Return true if every output model is an original."
-  [models]
-  (->> models
-       (keep get-output-type)
-       (every? #(= % :original))))
-
-(defn cap-output
-  "Capture and collapse all output tokens found in seqex. The final
-  output-tokens will be an initial model followed by any non-output models."
-  [seqex]
-  )
-
