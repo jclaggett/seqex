66a6ff0b9f844d07cd0e05e482bc3174282f8dd5
Jonathan Claggett <jclaggett@lonocloud.com>
2013-10-15 22:55:35 -0400

    [negset] Add new functionality: difference and more
    Add difference interset? disjoint? superset? subset? functions.
    Add equiv to NegSet.
    
    Clean up implementation to use reduce (using reduced).

--- src/n01se/negset.clj
+++ src/n01se/negset.clj
@@ -7,7 +7,7 @@
 (defprotocol Complement
   (complement [s] "Return the complement of s (U \\ s)"))
 
-(defprotocol Negative "Something not positive?" )
+(defprotocol Negative "A value defined in terms of what it is not" )
 
 (deftype NegSet [^clojure.lang.IPersistentSet non-elems]
   clojure.lang.IPersistentSet
@@ -15,8 +15,11 @@
   (disjoin [_ e] (NegSet. (conj non-elems e)))
   (cons [_ e] (NegSet. (disj non-elems e)))
 
-  (seq [_] (throw "Unable to seq a negative set"))
-  (count [_] (throw "Unable to count a negative set"))
+  (seq [_] (throw "Unable to seq a negative set (infinitely large)"))
+  (count [_] (throw "Unable to count a negative set (infinitely large)"))
+
+  (equiv [_ other] (and (instance? NegSet other)
+                        (= non-elems (.non-elems other))))
 
   Complement
   (complement [_] non-elems)
@@ -33,34 +36,90 @@
   Complement
   (complement [s] (NegSet. s)))
 
-(defn separate [pred coll]
-  ((juxt remove filter) pred coll))
+;; define s-* operators that only work with positive sets
+;; consider them a part of the clojure.set library
+(defn s-intersect?
+  "Does set1 intersect set2?"
+  [set1 set2]
+  (or (if (< (count set1) (count set2))
+        (some #(contains? set2 %) set1)
+        (some #(contains? set1 %) set2))
+      false))
 
-(defn separate-pos-neg [sets]
-  (separate (partial satisfies? Negative) sets))
+(defn s-disjoint?
+  "Is set1 disjoint from set2?"
+  [set1 set2]
+  (not (s-intersect? set1 set2)))
+
+;; Convenience macro for defining the behavior of various combinations
+;; of positive and negative sets.
+(defmacro case-sets [s1 s2 c1 c2 c3 c4]
+  `(case [(satisfies? Negative ~s1) (satisfies? Negative ~s2)]
+     [false false] ~c1
+     [false true ] ~c2
+     [true  false] ~c3
+     [true  true ] ~c4))
+
+(defn intersect?
+  [set1 set2]
+  (case-sets set1 set2
+    (s-intersect? set1 set2)
+    (not (s/subset? set1 (complement set2)))
+    (not (s/subset? set2 (complement set1)))
+    true)) ;; always intersect on finite computers
+
+(defn disjoint?
+  [set1 set2]
+  (not (intersect? set1 set2)))
+
+(defn subset? [set1 set2]
+  (case-sets set1 set2
+    (s/subset? set1 set2)
+    (not (s-intersect? set1 (complement set2)))
+    false ;; never a subset on finite computers
+    (s/subset? (complement set2) (complement set1))))
+
+(defn superset? [set1 set2]
+  (subset? set2 set1))
 
 (defn union [& sets]
-  (let [[pos neg :as both] (separate-pos-neg sets)]
-    (case (map empty? both)
-      [false false] (complement (s/difference
-                                  (apply s/intersection (map complement neg))
-                                  (apply s/union pos)))
-      [false true ] (apply s/union pos)
-      [true  false] (complement (apply s/intersection (map complement neg)))
-      [true  true ] nil))
-  )
+  (reduce
+   (fn
+     ([] nil)
+     ([set1 set2]
+        (if (= (complement #{}) set1)
+          (reduced set1)
+          (case-sets set1 set2
+            (s/union set1 set2)
+            (complement (s/difference (complement set2) set1))
+            (complement (s/difference (complement set1) set2))
+            (complement (s/intersection (complement set1) (complement set2)))))))
+   sets))
 
 (defn intersection [& sets]
-  (let [[pos neg :as both] (separate-pos-neg sets)]
-    (case (map empty? both)
-      [false false] (s/difference
-                      (apply s/intersection pos)
-                      (apply s/union (map complement neg)))
-      [false true ] (apply s/intersection pos)
-      [true  false] (complement (apply s/union (map complement neg)))
-      [true  true ] nil)))
+  (reduce
+   (fn
+     ([] nil)
+     ([set1 set2]
+        (if (= #{} set1)
+          (reduced set1)
+          (case-sets set1 set2
+            (s/intersection set1 set2)
+            (s/difference set1 (complement set2))
+            (s/difference set2 (complement set1))
+            (complement (s/union (complement set1) (complement set2)))))))
+   sets))
 
-(defn difference [a & sets]
-  )
-(defn subset? [a b])
-(defn superset? [a b])
+(defn difference [& sets]
+  (reduce
+   (fn
+     ([] nil)
+     ([set1 set2]
+        (if (= #{} set1)
+          (reduced set1)
+          (case-sets set1 set2
+            (s/difference set1 set2)
+            (s/intersection set1 (complement set2))
+            (complement (s/union (complement set1) set2))
+            (s/difference (complement set2) (complement set1))))))
+   sets))
