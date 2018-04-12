36717e6e8e1dbf7876e1ec9209dd916efb6acc32
Jonathan Claggett <jonathan.claggett@viasat.com>
2018-04-11 23:50:38 -0400

    version 2.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject n01se/seqex "2.0.2-SNAPSHOT"
+(defproject n01se/seqex "2.0.2"
   :description "Sequence Expressions."
   :url "http://github.com/jclaggett/seqex"
   :license {:name         "Eclipse Public License - v 1.0"
